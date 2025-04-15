398072da82e7188f65f1046d5a9b5ad90bbe26d6
Mike Prince <mike@mikeprince.com>
2025-04-14 23:37:58 -0700

    Update dependencies and extern loglevel, up to 0.4.10

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,5 +8,8 @@ export default defineConfig({
     sourcemap: true,
     clean: true,
     outDir: "dist",
-    target: "esnext"
+    target: "esnext",
+    external: [
+        "loglevel"
+    ]
 });
