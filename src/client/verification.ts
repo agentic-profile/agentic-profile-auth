604b2142fb37e3523f182c95644d43a5873e6bc1
Mike Prince <mike@mikeprince.com>
2025-10-27 17:07:54 -0700

    Expanded verification method resolution to verificationMethods

--- src/client/verification.ts
+++ src/client/verification.ts
@@ -42,11 +42,26 @@ export async function resolveVerificationKey( agentDid: DID, profileResolver: Pr
 
     let { profile, keyring } = await profileResolver( agentDid );
 
+    // Either the fragment matches a service, or a verification method in the same document
     const agent = profile.service?.find(e=>matchingFragmentIds( e.id, agentDid ) ) as AgentService;
-    if( !agent )
-        throw new Error("Failed to find agent service for " + agentDid );
+    if( !agent ) {
+        const vm = profile.verificationMethod?.find(e=>matchingFragmentIds( e.id, agentDid ) );
+        if( !vm )
+            throw new Error("Failed to find agent service or verification method for " + agentDid );
+
+        const resolution = await resolveIdOrMethod( vm, profile, keyring, profileResolver, agentDid );
+        if( resolution )
+            return resolution;
+        else
+            throw new Error("Failed to find agent service or verification method for " + agentDid );
+    }
 
     for( const idOrMethod of agent.capabilityInvocation ) {
+        const resolution = await resolveIdOrMethod( idOrMethod, profile, keyring, profileResolver, agentDid );
+        if( resolution )
+            return resolution;
+
+        /*
         let verificationId: FragmentID;
         let verificationMethod: VerificationMethod | undefined;
 
@@ -91,6 +106,7 @@ export async function resolveVerificationKey( agentDid: DID, profileResolver: Pr
             throw new Error(`Failed to find private key for ${agentDid} public key ${b64uPublicKey}` );
 
         return { verificationId, privateJwk };
+        */
     }
 
     throw new Error("Failed to find a verification key for " + agentDid );
@@ -98,4 +114,57 @@ export async function resolveVerificationKey( agentDid: DID, profileResolver: Pr
 
 function resolvePrivateJwk( keyring: JWKSet[], b64uPublicKey: Base64Url ) {
     return keyring.find(e=>e.b64uPublicKey===b64uPublicKey)?.privateJwk;
+}
+
+async function resolveIdOrMethod(
+    idOrMethod: string | VerificationMethod,
+    profile: AgenticProfile,
+    keyring: JWKSet[],
+    profileResolver: ProfileAndKeyringResolver,
+    agentDid: DID
+) {
+    let verificationId: FragmentID;
+    let verificationMethod: VerificationMethod | undefined;
+
+    if( typeof idOrMethod === 'string' ) {
+        verificationId = idOrMethod as DID;     // (might reference key in another DID Document!)
+
+        // follow to another document?
+        const documentId = removeFragmentId( verificationId );
+        if( documentId.length > 0 && documentId !== profile.id ) {
+            log.debug( `resolveVerificationKey() using linked profile ${documentId} of ${verificationId}` );
+            ({ profile, keyring } = await profileResolver( documentId ));
+        }
+
+        // Agentic profile requires verification method ids to be just fragments
+        const { fragmentId } = pruneFragmentId( verificationId );
+        if( !fragmentId )
+            throw new Error(`Failed to extract fragment id from ${verificationId} in ${profile.id}`);
+
+        const found = profile.verificationMethod?.find(e=>e.id === fragmentId);
+        if( !found ) {
+            log.warn( `INVALID agentic profile, verification method does not resolve for ${agentDid} verification id ${verificationId}` );
+            return undefined;   // invalid AgenticProfile... fix!  (and keep looking for now...)
+        }
+        verificationMethod = found;
+    } else if( typeof idOrMethod === 'object' ) {
+        verificationMethod = idOrMethod as VerificationMethod;
+        verificationId = verificationMethod.id;
+    } else
+        throw new Error(`INVALID agentic profile, verification method is of unknown type: ${typeof idOrMethod} for ${profile.id}`);
+
+    if( verificationMethod.type !== "JsonWebKey2020" ) {
+        log.warn( `Skipping unsupported verification type ${verificationMethod.type} for ${profile.id}` )
+        return undefined;
+    }
+
+    const b64uPublicKey = verificationMethod.publicKeyJwk?.x;    // Only for JsonWebKey2020!
+    if( !b64uPublicKey )
+        throw new Error(`Failed to find public key for ${agentDid} verificationMethod ${verificationId}` );
+
+    const privateJwk = resolvePrivateJwk( keyring, b64uPublicKey );
+    if( !privateJwk )
+        throw new Error(`Failed to find private key for ${agentDid} public key ${b64uPublicKey}` );
+
+    return { verificationId, privateJwk };
 }
